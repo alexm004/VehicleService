0fc242f1b928be0f189a54bdde0e789dd22bd8d3
quest <alex.mpaul@quest-global.com>
2022-01-17 21:28:51 +0530

    model name

--- app/src/main/java/com/example/vehicleservice/MainActivity.java
+++ app/src/main/java/com/example/vehicleservice/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.vehicleservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -18,24 +21,40 @@ import java.io.InputStreamReader;
 public class MainActivity extends AppCompatActivity {
 
     private String vehicleModel;
-    private Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btn);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                readModel();
-            }
-        });
+        readVehicleModel();
 
+        //readModel();
     }
 
-    private void readModel() {
+    private void readVehicleModel() {
+
+        try {
+            int ch;
+            StringBuilder builder = new StringBuilder();
+            FileInputStream fileInputStream = openFileInput("Vehicle_MODEL.txt");
+            while((ch = fileInputStream.read()) != -1) {
+                builder.append((char)ch);
+            }
+            Toast.makeText(getApplicationContext(), ""+builder, Toast.LENGTH_LONG).show();
+        }
+        catch (FileNotFoundException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    /*private void readModel() {
         String string = "";
         try {
             InputStream inputStream = getAssets().open("Vehicle_MODEL.txt");
@@ -47,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         Toast.makeText(MainActivity.this, ""+string, Toast.LENGTH_LONG).show();
-    }
+    }*/
+
 
 
 }
