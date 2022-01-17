247780dcec45a62320bc44b3c12f597da79193ce
quest <alex.mpaul@quest-global.com>
2022-01-17 18:10:28 +0530

    model name

--- app/src/main/java/com/example/vehicleservice/MainActivity.java
+++ app/src/main/java/com/example/vehicleservice/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.vehicleservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String vehicleModel;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readModel();
+            }
+        });
+
     }
+
+    private void readModel() {
+        String string = "";
+        try {
+            InputStream inputStream = getAssets().open("Vehicle_MODEL.txt");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            string = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(MainActivity.this, ""+string, Toast.LENGTH_LONG).show();
+    }
+
+
 }
